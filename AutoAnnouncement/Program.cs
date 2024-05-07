a6dc56cb82651ed6ee0a50635632d6199fb07e14
雨雨 宋 <s084088@163.com>
2024-05-07 09:23:45 +0800

    1

--- AutoAnnouncement/Program.cs
+++ AutoAnnouncement/Program.cs
@@ -12,21 +12,21 @@ JobManager.AddJob(() => SendMessage("《蛇岛赛马》活动将于5分钟后开
 JobManager.AddJob(() => SendMessage("《蛇岛赛马》活动将于15分钟后开始，可从各主城小狼处入场"), s => s.ToRunEvery(1).Days().At(21, 15));
 JobManager.AddJob(() => SendMessage("《蛇岛赛马》活动将于5分钟后开始，可从各主城小狼处入场"), s => s.ToRunEvery(1).Days().At(21, 25));
 
-JobManager.AddJob(() => SendMessage("《勇闯冥兽城》活动将于15分钟后开始，可从各主城小狼处入场"), s => s.ToRunEvery(1).Weeks().On(DayOfWeek.Monday).At(19, 45));
-JobManager.AddJob(() => SendMessage("《勇闯冥兽城》活动将于5分钟后开始，可从各主城小狼处入场"), s => s.ToRunEvery(1).Weeks().On(DayOfWeek.Monday).At(19, 55));
+JobManager.AddJob(() => SendMessage("《勇闯冥兽城》活动将于15分钟后开始，可从各主城小狼处入场"), s => s.ToRunEvery(0).Weeks().On(DayOfWeek.Monday).At(19, 45));
+JobManager.AddJob(() => SendMessage("《勇闯冥兽城》活动将于5分钟后开始，可从各主城小狼处入场"), s => s.ToRunEvery(0).Weeks().On(DayOfWeek.Monday).At(19, 55));
 
-JobManager.AddJob(() => SendMessage("《龙宫寻宝》活动将于15分钟后开始，可从各主城小狼处入场"), s => s.ToRunEvery(1).Weeks().On(DayOfWeek.Tuesday).At(19, 45));
-JobManager.AddJob(() => SendMessage("《龙宫寻宝》活动将于5分钟后开始，可从各主城小狼处入场"), s => s.ToRunEvery(1).Weeks().On(DayOfWeek.Tuesday).At(19, 55));
+JobManager.AddJob(() => SendMessage("《龙宫寻宝》活动将于15分钟后开始，可从各主城小狼处入场"), s => s.ToRunEvery(0).Weeks().On(DayOfWeek.Tuesday).At(19, 45));
+JobManager.AddJob(() => SendMessage("《龙宫寻宝》活动将于5分钟后开始，可从各主城小狼处入场"), s => s.ToRunEvery(0).Weeks().On(DayOfWeek.Tuesday).At(19, 55));
 
-JobManager.AddJob(() => SendMessage("《夺宝骑兵》活动将于15分钟后开始，可从祖龙城竞技场管理员处入场"), s => s.ToRunEvery(1).Weeks().On(DayOfWeek.Friday).At(17, 45));
-JobManager.AddJob(() => SendMessage("《夺宝骑兵》活动将于5分钟后开始，可从祖龙城竞技场管理员处入场"), s => s.ToRunEvery(1).Weeks().On(DayOfWeek.Friday).At(17, 55));
+JobManager.AddJob(() => SendMessage("《夺宝骑兵》活动将于15分钟后开始，可从祖龙城竞技场管理员处入场"), s => s.ToRunEvery(0).Weeks().On(DayOfWeek.Friday).At(17, 45));
+JobManager.AddJob(() => SendMessage("《夺宝骑兵》活动将于5分钟后开始，可从祖龙城竞技场管理员处入场"), s => s.ToRunEvery(0).Weeks().On(DayOfWeek.Friday).At(17, 55));
 
-JobManager.AddJob(() => SendMessage("《丛林遗迹》活动将于15分钟后开始，可从各主城小狼处入场"), s => s.ToRunEvery(1).Weeks().On(DayOfWeek.Sunday).At(14, 45));
-JobManager.AddJob(() => SendMessage("《丛林遗迹》活动将于15分钟后开始，可从各主城小狼处入场"), s => s.ToRunEvery(1).Weeks().On(DayOfWeek.Sunday).At(14, 55));
+JobManager.AddJob(() => SendMessage("《丛林遗迹》活动将于15分钟后开始，可从各主城小狼处入场"), s => s.ToRunEvery(0).Weeks().On(DayOfWeek.Sunday).At(14, 45));
+JobManager.AddJob(() => SendMessage("《丛林遗迹》活动将于15分钟后开始，可从各主城小狼处入场"), s => s.ToRunEvery(0).Weeks().On(DayOfWeek.Sunday).At(14, 55));
 
-JobManager.AddJob(() => SendMessage("《月度赛马》活动将于1小时后开始，即将停止报名！"), s => s.ToRunEvery(1).Months().On(28).At(18, 10));
-JobManager.AddJob(() => SendMessage("《月度赛马》活动将于20分钟后开始，可从各主城赛马点准备"), s => s.ToRunEvery(1).Months().On(28).At(18, 50));
-JobManager.AddJob(() => SendMessage("《月度赛马》活动将于5分钟后开始，可从各主城赛马点准备"), s => s.ToRunEvery(1).Months().On(28).At(19, 05));
+JobManager.AddJob(() => SendMessage("《月度赛马》活动将于1小时后开始，即将停止报名！"), s => s.ToRunEvery(0).Months().On(28).At(18, 10));
+JobManager.AddJob(() => SendMessage("《月度赛马》活动将于20分钟后开始，可从各主城赛马点准备"), s => s.ToRunEvery(0).Months().On(28).At(18, 50));
+JobManager.AddJob(() => SendMessage("《月度赛马》活动将于5分钟后开始，可从各主城赛马点准备"), s => s.ToRunEvery(0).Months().On(28).At(19, 05));
 
 JobManager.Start();
 
--- AutoAnnouncement/Tool.cs
+++ AutoAnnouncement/Tool.cs
@@ -24,6 +24,26 @@ internal static class Tool
                         deliveryDB.Connect("192.168.2.178", 29100);
                         deliveryDB.AddReceive<ChatBroadCast>(x => LogChatBroadCast(x.ToString(), x.Data));
                         deliveryDB.AddReceive<WorldChat>(x => LogChatBroadCast(x.ToString(), x.Data));
+
+                        deliveryDB.AddRecvedPacketsEvent(x =>
+                        {
+
+                            if (x.RecvPackets.Type == 0)                //解包失败,打印字节组
+                                Console.WriteLine(x.RecvPackets.Data.ToHexString());
+
+                            else if (x.RecvPackage == null)             //没写对应的处理包
+                                Console.WriteLine(x.RecvPackets.Type);
+
+                            else if (x.RecvPackage is AnnounceServerAttribute attribute)  //这可能是心跳包,不处理
+                            {
+                                //Console.WriteLine("接收到attribute包");
+                            }
+                            else                                                          //打印其他收到的包
+                            {
+                                //Console.WriteLine($"接收到数据包===={x.RecvPackage.GetType()}===={x.RecvPackage}");
+                            }
+                        });
+
                         Console.WriteLine($"{DateTime.Now:dd-HH:mm:ss}--服务器连接成功");
                     }
                     catch (Exception e)
@@ -53,9 +73,9 @@ internal static class Tool
 
     private static void LogChatBroadCast(string text, byte[] data)
     {
-        string hex = $"{DateTime.Now:HH:mm:ss}:  {data.ToHexString()}{Environment.NewLine}";
-        Console.Write(hex);
-        File.AppendAllText(DateTime.Now.ToString("MM_dd") + ".log", hex);
+        //string hex = $"{DateTime.Now:HH:mm:ss}:  {data.ToHexString()}{Environment.NewLine}";
+        //Console.Write(hex);
+        //File.AppendAllText(DateTime.Now.ToString("MM_dd") + ".log", hex);
 
         string log = $"{DateTime.Now:HH:mm:ss}:  {text}{Environment.NewLine}";
         Console.Write(log);
